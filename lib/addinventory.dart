cbf04e3f7a7d197aa8e02b71a4daf5e887054b4c
salwaiqlimaa <147375122+salwaiqlimaa@users.noreply.github.com>
2024-12-13 12:47:35 +0700

    Update addinventory.dart

--- lib/addinventory.dart
+++ lib/addinventory.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:stockit/homepage.dart';
+import 'package:stockitInventoryDetailScreen.dart';
 
 class AddInventoryScreen extends StatefulWidget {
   @override
@@ -22,8 +23,7 @@ class _AddInventoryScreenState extends State<AddInventoryScreen> {
     "Length": ["Kilometers (km)", "Meters (m)", "Centimeters (cm)", "Inches (in)", "Feet (ft)", "Other"],
   };
 
-  final List<String> categories = ["Add New Category", "Electronics", "Groceries","Fashion"];
-
+  final List<String> categories = ["Add New Category", "Electronics", "Groceries", "Fashion"];
   final List<String> currencies = ["EUR", "USD", "IDR", "JPY", "KRW", "AUD", "None"];
 
   @override
@@ -33,21 +33,13 @@ class _AddInventoryScreenState extends State<AddInventoryScreen> {
         backgroundColor: Colors.white,
         centerTitle: true,
         elevation: 0,
-        title: const Text(
-          "Add Inventory",
-          style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
-        ),
+        title: const Text("Add Inventory", style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold)),
         leading: IconButton(
-  icon: const Icon(Icons.arrow_back, color: Colors.black),
-  onPressed: () {
-    // Arahkan pengguna kembali ke Homepage
-    Navigator.pushReplacement(
-      context,
-      MaterialPageRoute(builder: (context) => Homepage()), // Ganti 'Homepage' dengan halaman yang sesuai
-    );
-  },
-),
-
+          icon: const Icon(Icons.arrow_back, color: Colors.black),
+          onPressed: () {
+            // Handle back button
+          },
+        ),
       ),
       body: Container(
         color: Colors.grey[100],
@@ -55,120 +47,9 @@ class _AddInventoryScreenState extends State<AddInventoryScreen> {
           padding: const EdgeInsets.all(16.0),
           child: ListView(
             children: [
-              buildTextField("Product Name", "Product Name"),
-              buildTextField("SKU/Product Code", "SKU/Product Code"),
-              buildTextField("Enter Barcode", "000000000"),
-              buildDropdown(
-                "Select a unit of measure",
-                selectedUnitCategory,
-                unitOptions.keys.toList(),
-                (value) {
-                  setState(() {
-                    selectedUnitCategory = value ?? selectedUnitCategory;
-                    selectedUnit = unitOptions[selectedUnitCategory]?.first ?? "Pieces (pcs)";
-                  });
-                },
-              ),
-              buildDropdown(
-                "Select Variety",
-                selectedUnit,
-                unitOptions[selectedUnitCategory]!,
-                (value) {
-                  if (value == "Other") {
-                    _showInputDialog(
-                      context,
-                      "Add a new unit for $selectedUnitCategory",
-                      (newValue) {
-                        setState(() {
-                          unitOptions[selectedUnitCategory]?.add(newValue);
-                          selectedUnit = newValue;
-                        });
-                      },
-                    );
-                  } else {
-                    setState(() {
-                      selectedUnit = value ?? selectedUnit;
-                    });
-                  }
-                },
-              ),
-              buildQuantityField("Initial Quantity", initialQuantity, (newQuantity) {
-                setState(() {
-                  initialQuantity = newQuantity;
-                });
-              }),
-              buildQuantityField("Reorder Quantity", reorderQuantity, (newQuantity) {
-                setState(() {
-                  reorderQuantity = newQuantity;
-                });
-              }),
-              buildTextField("Cost Price", "Cost Price"),
-              buildTextField("Selling Price", "Selling Price"),
-              buildDropdown(
-                "Select Currency",
-                selectedCurrency,
-                currencies,
-                (value) {
-                  setState(() {
-                    selectedCurrency = value ?? selectedCurrency;
-                  });
-                },
-              ),
-              buildDropdown(
-                "Select Category",
-                selectedCategory,
-                categories,
-                (value) {
-                  if (value == "Add New Category") {
-                    _showInputDialog(
-                      context,
-                      "Add New Category",
-                      (newValue) {
-                        setState(() {
-                          categories.add(newValue);
-                          selectedCategory = newValue;
-                        });
-                      },
-                    );
-                  } else {
-                    setState(() {
-                      selectedCategory = value ?? selectedCategory;
-                    });
-                  }
-                },
-              ),
+              ..._buildFormFields(),
               const SizedBox(height: 16),
-              ElevatedButton(
-                onPressed: () {
-                  showDialog(
-                    context: context,
-                    builder: (context) => AlertDialog(
-                      title: const Icon(Icons.check_circle, size: 40, color: Colors.green),
-                      content: const Text("Product added successfully"),
-                      actions: [
-                        TextButton(
-                          onPressed: () => Navigator.pop(context),
-                          child: const Text("See Inventory"),
-                        ),
-                        TextButton(
-                          onPressed: () => Navigator.pop(context),
-                          child: const Text("OK"),
-                        ),
-                      ],
-                    ),
-                  );
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: const Color(0xFF006A67),
-                  foregroundColor: Colors.white,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(22),
-                  ),
-                  padding: const EdgeInsets.all(16),
-                  textStyle: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                ),
-                child: const Text("Save Inventory"),
-              ),
+              _buildSaveButton(context),
             ],
           ),
         ),
@@ -176,61 +57,154 @@ class _AddInventoryScreenState extends State<AddInventoryScreen> {
     );
   }
 
-  Widget buildTextField(String label, String hint) {
-    return Padding(
-      padding: const EdgeInsets.only(bottom: 16.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(label, style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w500)),
-          const SizedBox(height: 8),
-          TextField(
-            decoration: InputDecoration(
-              hintText: hint,
-              filled: true,
-              fillColor: Colors.white,
-              border: _outlineInputBorder(),
-              focusedBorder: _outlineInputBorder(focused: true),
-            ),
-          ),
-        ],
+  List<Widget> _buildFormFields() {
+    return [
+      _buildTextField("Product Name", "Product Name"),
+      _buildTextField("SKU/Product Code", "SKU/Product Code"),
+      _buildTextField("Enter Barcode", "000000000"),
+      _buildDropdown("Select a unit of measure", selectedUnitCategory, unitOptions.keys.toList(), (value) {
+        setState(() {
+          selectedUnitCategory = value ?? selectedUnitCategory;
+          selectedUnit = unitOptions[selectedUnitCategory]?.first ?? "Pieces (pcs)";
+        });
+      }),
+      _buildDropdown("Select Variety", selectedUnit, unitOptions[selectedUnitCategory]!, (value) {
+        if (value == "Other") {
+          _showInputDialog(context, "Add a new unit for $selectedUnitCategory", (newValue) {
+            setState(() {
+              unitOptions[selectedUnitCategory]?.add(newValue);
+              selectedUnit = newValue;
+            });
+          });
+        } else {
+          setState(() {
+            selectedUnit = value ?? selectedUnit;
+          });
+        }
+      }),
+      _buildQuantityField("Initial Quantity", initialQuantity, (newQuantity) {
+        setState(() {
+          initialQuantity = newQuantity;
+        });
+      }),
+      _buildQuantityField("Reorder Quantity", reorderQuantity, (newQuantity) {
+        setState(() {
+          reorderQuantity = newQuantity;
+        });
+      }),
+      _buildTextField("Cost Price", "Cost Price"),
+      _buildTextField("Selling Price", "Selling Price"),
+      _buildDropdown("Select Currency", selectedCurrency, currencies, (value) {
+        setState(() {
+          selectedCurrency = value ?? selectedCurrency;
+        });
+      }),
+      _buildDropdown("Select Category", selectedCategory, categories, (value) {
+        if (value == "Add New Category") {
+          _showInputDialog(context, "Add New Category", (newValue) {
+            setState(() {
+              categories.add(newValue);
+              selectedCategory = newValue;
+            });
+          });
+        } else {
+          setState(() {
+            selectedCategory = value ?? selectedCategory;
+          });
+        }
+      }),
+    ];
+  }
+
+  Widget _buildTextField(String label, String hint) {
+    return _buildFieldContainer(
+      label: label,
+      child: TextField(
+        decoration: InputDecoration(
+ hintText: hint,
+          filled: true,
+          fillColor: Colors.white,
+          border: _outlineInputBorder(),
+          focusedBorder: _outlineInputBorder(focused: true),
+        ),
       ),
     );
   }
 
-  Widget buildDropdown(
-      String label, String value, List<String> items, ValueChanged<String?> onChanged) {
-    return Padding(
-      padding: const EdgeInsets.only(bottom: 16.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(label, style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w500)),
-          const SizedBox(height: 8),
-          DropdownButtonFormField<String>(
-            value: value,
-            onChanged: onChanged,
-            items: items.map((item) {
-              return DropdownMenuItem<String>(
-                value: item,
-                child: Text(item),
-              );
-            }).toList(),
-            decoration: InputDecoration(
-              filled: true,
-              fillColor: Colors.white,
-              border: _outlineInputBorder(),
-              focusedBorder: _outlineInputBorder(focused: true),
-            ),
-          ),
-        ],
+  Widget _buildDropdown(String label, String value, List<String> items, ValueChanged<String?> onChanged) {
+    return _buildFieldContainer(
+      label: label,
+      child: DropdownButtonFormField<String>(
+        value: value,
+        onChanged: onChanged,
+        items: items.map((item) {
+          return DropdownMenuItem<String>(
+            value: item,
+            child: Text(item),
+          );
+        }).toList(),
+        decoration: InputDecoration(
+          filled: true,
+          fillColor: Colors.white,
+          border: _outlineInputBorder(),
+          focusedBorder: _outlineInputBorder(focused: true),
+        ),
       ),
     );
   }
 
-  Widget buildQuantityField(String label, int quantity, ValueChanged<int> onChanged) {
+  Widget _buildQuantityField(String label, int quantity, ValueChanged<int> onChanged) {
     final TextEditingController controller = TextEditingController(text: quantity.toString());
 
+    return _buildFieldContainer(
+      label: label,
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(22.0),
+          border: Border.all(color: Colors.grey, width: 1.0),
+        ),
+        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: Row(
+          children: [
+            IconButton(
+              icon: const Icon(Icons.remove),
+              onPressed: () {
+                if (quantity > 0) {
+                  onChanged(quantity - 1);
+                }
+              },
+            ),
+            Expanded(
+              child: TextField(
+                controller: controller,
+                keyboardType: TextInputType.number,
+                textAlign: TextAlign.center,
+                decoration: const InputDecoration(
+                  border: InputBorder.none,
+                  hintText: "0",
+                ),
+                onChanged: (value) {
+                  final int? newQuantity = int.tryParse(value);
+                  if (newQuantity != null) {
+                    onChanged(newQuantity);
+                  }
+                },
+              ),
+            ),
+            IconButton(
+              icon: const Icon(Icons.add),
+              onPressed: () {
+                onChanged(quantity + 1);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildFieldContainer({required String label, required Widget child}) {
     return Padding(
       padding: const EdgeInsets.only(bottom: 16.0),
       child: Column(
@@ -238,58 +212,51 @@ class _AddInventoryScreenState extends State<AddInventoryScreen> {
         children: [
           Text(label, style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w500)),
           const SizedBox(height: 8),
-          Container(
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(22.0),
-              border: Border.all(
-                color: Colors.grey,
-                width: 1.0,
-              ),
-            ),
-            padding: const EdgeInsets.symmetric(horizontal: 8.0),
-            child: Row(
-              children: [
-                IconButton(
-                  icon: const Icon(Icons.remove),
-                  onPressed: () {
-                    if (quantity > 0) {
-                      onChanged(quantity - 1);
-                    }
-                  },
-                ),
-                Expanded(
-                  child: TextField(
-                    controller: controller,
-                    keyboardType: TextInputType.number,
-                    textAlign: TextAlign.center,
-                    decoration: const InputDecoration(
-                      border: InputBorder.none, // Hilangkan border bawaan TextField
-                      hintText: "0",
-                    ),
-                    onChanged: (value) {
-                      final int? newQuantity = int.tryParse(value);
-                      if (newQuantity != null) {
-                        onChanged(newQuantity);
-                      }
-                    },
-                  ),
-                ),
-                IconButton(
-                  icon: const Icon(Icons.add),
-                  onPressed: () {
-                    onChanged(quantity + 1);
-                  },
-                ),
-              ],
-            ),
-          ),
+          child,
         ],
       ),
     );
   }
 
-
+  Widget _buildSaveButton(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {
+        showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: const Icon(Icons.check_circle, size: 40, color: Colors.green),
+            content: const Text("Product added successfully"),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                    Navigator.push(
+                        context,
+                          MaterialPageRoute(builder: (context) => InventoryDetailScreen()),
+                          );
+                        },
+                        child: const Text("See Inventory"),
+                        ),
+              TextButton(
+                onPressed: () => Navigator.pop(context),
+                child: const Text("OK"),
+              ),
+            ],
+          ),
+        );
+      },
+      style: ElevatedButton.styleFrom(
+        backgroundColor: const Color(0xFF006A67),
+        foregroundColor: Colors.white,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(22),
+        ),
+        padding: const EdgeInsets.all(16),
+        textStyle: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+      ),
+      child: const Text("Save Inventory"),
+    );
+  }
 
   void _showInputDialog(BuildContext context, String title, ValueChanged<String> onSubmitted) {
     final controller = TextEditingController();
